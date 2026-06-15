b7da68c7c88dc5e5ee6e62f72f8faf0c0e86d718
Jokeren1 <inhakim999@gmail.com>
2026-06-15 15:43:33 -0500

    Implemented Yi Sang's IDs in data and added images

---
+++ ts/data.ts
@@ -0,0 +1,38 @@
+import { SinnerID } from "./type";
+
+export const yiSangIDs: SinnerID[] = [
+    { sinner: "Yi Sang", idName: "LCB Sinner", image: "YiSang/LCB_Yi_Sang.webp", rarity: "0" },
+    { sinner: "Yi Sang", idName: "Dieci Assoc. South Section 4", image: "YiSang/DS4_YiSang.webp", rarity: "00" },
+    { sinner: "Yi Sang", idName: "LCE E.G.O::Lantern", image: "YiSang/LCE_YiSang.webp", rarity: "00" },
+    { sinner: "Yi Sang", idName: "Molar Office Fixer", image: "YiSang/MOF_YiSang.webp", rarity: "00" },
+    { sinner: "Yi Sang", idName: "The Pequod First Mate", image: "YiSang/TPF_YiSang.webp", rarity: "00" },
+    { sinner: "Yi Sang", idName: "Seven Assoc. South Section 6", image: "YiSang/SA6_YiSang.webp", rarity: "00" },
+    { sinner: "Yi Sang", idName: "Blade Lineage Salsu", image: "YiSang/BL_YiSang.webp", rarity: "000" },
+    { sinner: "Yi Sang", idName: "Effloresced E.G.O::SpiceBush", image: "YiSang/Spicebush_YiSang.webp", rarity: "000" },
+    { sinner: "Yi Sang", idName: "Heishou Pack - Wu Branch Adept Yi Sang", image: "YiSang/Heishou_YiSang.webp", rarity: "000" },
+    { sinner: "Yi Sang", idName: "The House of Spiders - The Index Nursefather", image: "YiSang/Index_YiSang.webp", rarity: "000" },
+    { sinner: "Yi Sang", idName: "Liu Assoc. South Section 3", image: "YiSang/LS3_YiSang.webp", rarity: "000" },
+    { sinner: "Yi Sang", idName: "Lobotomy E.G.O::Solemn Lament", image: "YiSang/Solemn_YiSang.webp", rarity: "000" },
+    { sinner: "Yi Sang", idName: "N Corp. E.G.O::Fell Bullet", image: "YiSang/Fell_YiSang.webp", rarity: "000" },
+    { sinner: "Yi Sang", idName: "The Ring Pointillist Student", image: "YiSang/Ring_YiSang.webp", rarity: "000" },
+    { sinner: "Yi Sang", idName: "W Corp. L3 Cleanup Agent", image: "YiSang/WCorp_YiSang.webp", rarity: "000" },
+];
+
+export const faustIDs: SinnerID[] = [
+    { sinner: "Faust", idName: "LCB Sinner", image: "Faust/LCB_Faust.webp", rarity: "0" },
+    { sinner: "Faust", idName: "Lobotomy Corp. Remnant", image: "Faust/LCR_Faust.webp", rarity: "00" },
+    { sinner: "Faust", idName: "W Corp. L2 Cleanup Agent", image: "Faust/WCorp_Faust.webp", rarity: "00" },
+    { sinner: "Faust", idName: "Wuthering Heights Butler", image: "Faust/WH_Faust.webp", rarity: "00" },
+    { sinner: "Faust", idName: "Zwei Assoc. South Section 4", image: "Faust/ZS4_Faust.webp", rarity: "00" },
+    
+    { sinner: "Faust", idName: "The One Who Grips", image: "Faust/Grips_Faust.webp", rarity: "000" },
+    { sinner: "Faust", idName: "Seven Assoc. South Section 4", image: "Faust/SA4_Faust.webp", rarity: "000" },
+    { sinner: "Faust", idName: "Lobotomy E.G.O::Regret", image: "Faust/Regret_Faust.webp", rarity: "000" },
+    { sinner: "Faust", idName: "Blade Lineage Salsu", image: "Faust/BL_Faust.webp", rarity: "000" },
+    
+];
+
+export const donIDs: SinnerID[] = [
+    { sinner: "Don", idName: "LCB Sinner", image: "Don/LCB_Don.webp", rarity: "0" },
+
+];
