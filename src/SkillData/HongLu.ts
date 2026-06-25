44f8d7c79a8556e38ca5293527578257ba104663
Jokeren1 <inhakim999@gmail.com>
2026-06-25 11:08:35 -0500

    Implemented Hong Lu's skills and added voicelines

--- src/SkillData/HongLu.ts
+++ src/SkillData/HongLu.ts
@@ -2,93 +2,104 @@ import type { Skill } from "../type.js";
 
 export const hongluskillData = {
     LCBHongLu: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Downward Cleave", icon: "IDs/HongLu/Icons/LCB/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Dual Strike", icon: "IDs/HongLu/Icons/LCB/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Whirlwind", icon: "IDs/HongLu/Icons/LCB/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Evade", icon: "IDs/HongLu/Icons/LCB/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     FanghuntHongLu: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Slam", icon: "IDs/HongLu/Icons/Fanghunt/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Bonecrusher", icon: "IDs/HongLu/Icons/Fanghunt/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "A Cheerful Hunt's End", icon: "IDs/HongLu/Icons/Fanghunt/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Emergency Styptics", icon: "IDs/HongLu/Icons/Fanghunt/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     HookHongLu: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Track", icon: "IDs/HongLu/Icons/Hook/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Goin' First", icon: "IDs/HongLu/Icons/Hook/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Rampage", icon: "IDs/HongLu/Icons/Hook/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Quickness", icon: "IDs/HongLu/Icons/Hook/Defense.webp", skillNum: "Defense" }
     ]  satisfies Skill[],
     KurokumoHongLu: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Cleave", icon: "IDs/HongLu/Icons/Kurokumo/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Cloud Cutter", icon: "IDs/HongLu/Icons/Kurokumo/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Cloudburst", icon: "IDs/HongLu/Icons/Kurokumo/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Guard", icon: "IDs/HongLu/Icons/Kurokumo/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     LiuHongLu: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Warm Up", icon: "IDs/HongLu/Icons/Liu/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Flowing Flame", icon: "IDs/HongLu/Icons/Liu/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Crimson Blaze Fist", icon: "IDs/HongLu/Icons/Liu/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Guard", icon: "IDs/HongLu/Icons/Liu/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     WCorpHongLu: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Energy Cycle", icon: "IDs/HongLu/Icons/WCorp/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Cleanup Support", icon: "IDs/HongLu/Icons/WCorp/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Deploy Charge Barrier", icon: "IDs/HongLu/Icons/WCorp/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Guard", icon: "IDs/HongLu/Icons/WCorp/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     DieciHongLu: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Expend Knowledge", icon: "IDs/HongLu/Icons/Dieci/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Unveil", icon: "IDs/HongLu/Icons/Dieci/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Cyclical Knowledge", icon: "IDs/HongLu/Icons/Dieci/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Guiding Steps", icon: "IDs/HongLu/Icons/Dieci/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     YurodivyHongLu: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Deduction Start", icon: "IDs/HongLu/Icons/Yurodivy/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Morph Cane Technique", icon: "IDs/HongLu/Icons/Yurodivy/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "You're the Culprit!", icon: "IDs/HongLu/Icons/Yurodivy/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Wait?!", icon: "IDs/HongLu/Icons/Yurodivy/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     FullStopHongLu: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Take the Shot", icon: "IDs/HongLu/Icons/FullStop/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Melee Support", icon: "IDs/HongLu/Icons/FullStop/Skill1_2.webp", skillNum: "Skill 1-2" },
+        { skillName: "Target Marked", icon: "IDs/HongLu/Icons/FullStop/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Combo: Target Marked", icon: "IDs/HongLu/Icons/FullStop/Skill2_2.webp", skillNum: "Skill 2-2" },
+        { skillName: "Full-Stop to Life", icon: "IDs/HongLu/Icons/FullStop/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Combo: Full-Stop to Life", icon: "IDs/HongLu/Icons/FullStop/Skill3_2.webp", skillNum: "Skill 3-2" },
+        { skillName: "Combat Poise", icon: "IDs/HongLu/Icons/FullStop/Defense.webp", skillNum: "Defense" },
+        { skillName: "Beyond the Shadow", icon: "IDs/HongLu/Icons/FullStop/Defense2.webp", skillNum: "Defense 2" }
     ] satisfies Skill[],
     RingHongLu: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Anatomize", icon: "IDs/HongLu/Icons/Ring/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "My, How We Love the Flesh and the Blood", icon: "IDs/HongLu/Icons/Ring/Skill1_2.webp", skillNum: "Skill 1-2" },
+        { skillName: "Gather Ingredient - Blood-bathed Objet", icon: "IDs/HongLu/Icons/Ring/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Installation Art no. 3: Improvised Ribcage", icon: "IDs/HongLu/Icons/Ring/Skill2_2.webp", skillNum: "Skill 2-2" },
+        { skillName: "Tibia's Melody - Anatomization of the Unanatomized by the Anatomized", icon: "IDs/HongLu/Icons/Ring/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Closing Time - Installation Art no. 1: Your Flesh and Bones as the Gallery's Seats", icon: "IDs/HongLu/Icons/Ring/Skill3_2.webp", skillNum: "Skill 3-2" },
+        { skillName: "Curating the Exhibition", icon: "IDs/HongLu/Icons/Ring/Defense.webp", skillNum: "Defense" },
+        { skillName: "Please. Vandalism is Prohibited.", icon: "IDs/HongLu/Icons/Ring/Defense2.webp", skillNum: "Defense 2" }
     ] satisfies Skill[],
     KCorpHongLu: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Impede the Intruder", icon: "IDs/HongLu/Icons/KCorp/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Decay Blade", icon: "IDs/HongLu/Icons/KCorp/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Excise Target", icon: "IDs/HongLu/Icons/KCorp/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Ampule Injection", icon: "IDs/HongLu/Icons/KCorp/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     LordHongLu: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "I Wish to Open the Path", icon: "IDs/HongLu/Icons/Lord/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Tarnished Blood's Absolute Cleaver of Ambitions [汚血絕志竟成]", icon: "IDs/HongLu/Icons/Lord/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Answer Me, Heishou Packs", icon: "IDs/HongLu/Icons/Lord/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "I Carve the Path of a Lord", icon: "IDs/HongLu/Icons/Lord/Skill3_2.webp", skillNum: "Skill 3-2" },
+        { skillName: "Lonesome Stand: Sacrifice to Claim The Garden [孑孑單身，捨生取园]", icon: "IDs/HongLu/Icons/Lord/3_3.webp", skillNum: "Skill 3-3" },
+        { skillName: "Heishou Keenclaw [黑獸利爪]", icon: "IDs/HongLu/Icons/Lord/Defense.webp", skillNum: "Defense" },
+        { skillName: "Lordsguard", icon: "IDs/HongLu/Icons/Lord/Defense2.webp", skillNum: "Defense 2" }
     ] satisfies Skill[],
     RCorpHongLu: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Energy Strike", icon: "IDs/HongLu/Icons/RCorp/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Mind Whip", icon: "IDs/HongLu/Icons/RCorp/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Concentration", icon: "IDs/HongLu/Icons/RCorp/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Energy Condensation", icon: "IDs/HongLu/Icons/RCorp/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     SCorpHongLu: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Constricting Ohra Bonds", icon: "IDs/HongLu/Icons/SCorp/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Avulsion Slash", icon: "IDs/HongLu/Icons/SCorp/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Hunter's Scouring", icon: "IDs/HongLu/Icons/SCorp/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Throwing the Ohra Bond", icon: "IDs/HongLu/Icons/SCorp/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     TingtangHongLu: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Throat Slit", icon: "IDs/HongLu/Icons/Tingtang/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Shank", icon: "IDs/HongLu/Icons/Tingtang/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Mutilate", icon: "IDs/HongLu/Icons/Tingtang/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Evade", icon: "IDs/HongLu/Icons/Tingtang/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[]
 };
