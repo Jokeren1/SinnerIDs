90d8222156ff50b2ff04e07819ab371d30743ead
Jokeren1 <inhakim999@gmail.com>
2026-06-25 12:35:41 -0500

    Implemented Heathcliff's skills and added voiceline

--- src/SkillData/Heathcliff.ts
+++ src/SkillData/Heathcliff.ts
@@ -2,99 +2,106 @@ import type { Skill } from "../type.js";
 
 export const heathcliffskillData = {
     LCBHeathcliff: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Bat Bash", icon: "IDs/Heathcliff/Icons/LCB/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Smackdown", icon: "IDs/Heathcliff/Icons/LCB/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Upheaval", icon: "IDs/Heathcliff/Icons/LCB/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Counter", icon: "IDs/Heathcliff/Icons/LCB/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     MultiCrackHeathcliff: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "40S-2 Activation", icon: "IDs/Heathcliff/Icons/MultiCrack/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Photoelectric Mark", icon: "IDs/Heathcliff/Icons/MultiCrack/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Photoelectric Harpoon", icon: "IDs/Heathcliff/Icons/MultiCrack/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Charge", icon: "IDs/Heathcliff/Icons/MultiCrack/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     NCorpHeathcliff: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Gawky Nailing", icon: "IDs/Heathcliff/Icons/NCorp/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Puri...fy!", icon: "IDs/Heathcliff/Icons/NCorp/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Infirm Retribution", icon: "IDs/Heathcliff/Icons/NCorp/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Guard", icon: "IDs/Heathcliff/Icons/NCorp/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     SevenHeathcliff: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Intuition", icon: "IDs/Heathcliff/Icons/Seven/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "The Wrap-Up", icon: "IDs/Heathcliff/Icons/Seven/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Forensics", icon: "IDs/Heathcliff/Icons/Seven/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Guard", icon: "IDs/Heathcliff/Icons/Seven/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     ShiHeathcliff: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Extreme Edge", icon: "IDs/Heathcliff/Icons/Shi/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Flying Sword", icon: "IDs/Heathcliff/Icons/Shi/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Flashing Strike", icon: "IDs/Heathcliff/Icons/Shi/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Counter", icon: "IDs/Heathcliff/Icons/Shi/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     FullStopHeathcliff: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Return Fire", icon: "IDs/Heathcliff/Icons/FullStop/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Headshot", icon: "IDs/Heathcliff/Icons/FullStop/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Goin' for the Bullseye", icon: "IDs/Heathcliff/Icons/FullStop/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "AL-HV Pulverisation Round", icon: "IDs/Heathcliff/Icons/FullStop/Skill3_2.webp", skillNum: "Skill 3-2" },
+        { skillName: "Combat Poise", icon: "IDs/Heathcliff/Icons/FullStop/Defense.webp", skillNum: "Defense" },
+        { skillName: "Taking Cover", icon: "IDs/Heathcliff/Icons/FullStop/Defense2.webp", skillNum: "Defense 2" }
     ] satisfies Skill[],
     HeishouHeathcliff: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Peck 'em", icon: "IDs/Heathcliff/Icons/Heishou/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Mutilating Talons", icon: "IDs/Heathcliff/Icons/Heishou/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Bloodflame Massacre [血炎亂舞]", icon: "IDs/Heathcliff/Icons/Heishou/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Rooster's Rampaging Blades Under the Ensanguined Heaven [血天下雞舞亂刀]", icon: "IDs/Heathcliff/Icons/Heishou/Skill3_2.webp", skillNum: "Skill 3-2" },
+        { skillName: "More Grub for Us", icon: "IDs/Heathcliff/Icons/Heishou/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
-    ThumbHeathcliff: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+    ThumbHeathcliff: [ // Replace placeholder icons with actual skill icons when they are released
+        { skillName: "La Soada di Palermo", icon: "Concept/WIP.png", skillNum: "Skill 1" },
+        { skillName: "Colpi Sottani", icon: "Concept/WIP.png", skillNum: "Skill 2" },
+        { skillName: "Il Cacciatore", icon: "Concept/WIP.png", skillNum: "Skill 3" },
+        { skillName: "Sezionatura di Cervo", icon: "Concept/WIP.png", skillNum: "Skill 3-2" },
+        { skillName: "Measuring Blade Reach", icon: "Concept/WIP.png", skillNum: "Defense" },
+        { skillName: "I Will Protect.", icon: "Concept/WIP.png", skillNum: "Defense 2" }
     ] satisfies Skill[],
     KurokumoHeathcliff: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Lenticular Rend", icon: "IDs/Heathcliff/Icons/Kurokumo/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Stormcloud", icon: "IDs/Heathcliff/Icons/Kurokumo/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Thundercleave", icon: "IDs/Heathcliff/Icons/Kurokumo/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Rules of the Backstreets", icon: "IDs/Heathcliff/Icons/Kurokumo/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     SunshowerHeathcliff: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Umbrella Thwack", icon: "IDs/Heathcliff/Icons/Sunshower/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Puddle Stomp", icon: "IDs/Heathcliff/Icons/Sunshower/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Spread Out!", icon: "IDs/Heathcliff/Icons/Sunshower/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Warning", icon: "IDs/Heathcliff/Icons/Sunshower/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     MiddleHeathcliff: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Kicking", icon: "IDs/Heathcliff/Icons/Middle/Skill1.png", skillNum: "Skill 1" },
+        { skillName: "Watch Your Back!", icon: "IDs/Heathcliff/Icons/Middle/Skill2.png", skillNum: "Skill 2" },
+        { skillName: "MY HAIR COUPOOOOOOOOOOONS!!!!", icon: "IDs/Heathcliff/Icons/Middle/Skill3.png", skillNum: "Skill 3" },
+        { skillName: "COMPLETE AND TOTAL EXTERMINATION!!", icon: "IDs/Heathcliff/Icons/Middle/3_2.png", skillNum: "Skill 3-2" },
+        { skillName: "Payback", icon: "IDs/Heathcliff/Icons/Middle/Defense.png", skillNum: "Defense" }
     ] satisfies Skill[],
     PequodHeathcliff: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Stalk Prey", icon: "IDs/Heathcliff/Icons/Pequod/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Snagharpoon", icon: "IDs/Heathcliff/Icons/Pequod/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Sever Knot", icon: "IDs/Heathcliff/Icons/Pequod/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Payback", icon: "IDs/Heathcliff/Icons/Pequod/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],    
     RCorpHeathcliff: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Graze the Grass", icon: "IDs/Heathcliff/Icons/RCorp/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Concentrated Fire", icon: "IDs/Heathcliff/Icons/RCorp/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Quick Suppression", icon: "IDs/Heathcliff/Icons/RCorp/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Evade", icon: "IDs/Heathcliff/Icons/RCorp/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     WCorpHeathcliff: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Congestion Cleanup", icon: "IDs/Heathcliff/Icons/WCorp/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Boost: Elbow Blade", icon: "IDs/Heathcliff/Icons/WCorp/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Rip Space - CCA Overdrive", icon: "IDs/Heathcliff/Icons/WCorp/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "CCA Overcharge", icon: "IDs/Heathcliff/Icons/WCorp/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     WildHeathcliff: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Beheading", icon: "IDs/Heathcliff/Icons/Wild/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Memorial Procession", icon: "IDs/Heathcliff/Icons/Wild/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Requiem", icon: "IDs/Heathcliff/Icons/Wild/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Lament, Mourn, and Despair", icon: "IDs/Heathcliff/Icons/Wild/Skill3_2.webp", skillNum: "Skill 3-2" },
+        { skillName: "O Dullahan...!", icon: "IDs/Heathcliff/Icons/Wild/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     OufiHeathcliff: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Execution Advised", icon: "IDs/Heathcliff/Icons/Oufi/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Final Warning", icon: "IDs/Heathcliff/Icons/Oufi/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Execution Sentencing", icon: "IDs/Heathcliff/Icons/Oufi/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Do Not Obstruct", icon: "IDs/Heathcliff/Icons/Oufi/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[]
 };
