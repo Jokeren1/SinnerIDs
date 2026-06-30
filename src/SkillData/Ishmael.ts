81800893cc8eda9ec89b11e65dc419d8804e6698
Jokeren1 <156864451+Jokeren1@users.noreply.github.com>
2026-06-29 20:27:00 -0500

    Ishmael WIP2

--- src/SkillData/Ishmael.ts
+++ src/SkillData/Ishmael.ts
@@ -14,34 +14,35 @@ export const ishmaelskillData = {
         { skillName: "Evade", icon: "IDs/Ishmael/Icons/Butler/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     LCCBIshmael: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Shove", icon: "IDs/Ishmael/Icons/LCCB/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Quake Rounds", icon: "IDs/Ishmael/Icons/LCCB/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Suppress", icon: "IDs/Ishmael/Icons/LCCB/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Guard", icon: "IDs/Ishmael/Icons/LCCB/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     SloshingIshmael: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "It's Heavy...!", icon: "IDs/Ishmael/Icons/Sloshing/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "It's Churning...!", icon: "IDs/Ishmael/Icons/Sloshing/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Corrosive Splash", icon: "IDs/Ishmael/Icons/Sloshing/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Guard", icon: "IDs/Ishmael/Icons/Sloshing/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     ShiIshmael: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Flying Sword", icon: "IDs/Ishmael/Icons/Shi/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Flashing Strike", icon: "IDs/Ishmael/Icons/Shi/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Catch Breath", icon: "IDs/Ishmael/Icons/Shi/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Counter", icon: "IDs/Ishmael/Icons/Shi/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     FamilyIshmael: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Crimson Strike", icon: "IDs/Ishmael/Icons/Family/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Crimson Strike - Launcher [始]", icon: "IDs/Ishmael/Icons/Family/Skill1_2.webp", skillNum: "Skill 1-2" },
+        { skillName: "Crimson Phoenix", icon: "IDs/Ishmael/Icons/Family/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Crimson Spring", icon: "IDs/Ishmael/Icons/Family/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Adept Evasion", icon: "IDs/Ishmael/Icons/Family/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     MiddleIshmael: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Totally Badass Ass-kicking", icon: "IDs/Ishmael/Icons/Middle/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Crossing Dervish", icon: "IDs/Ishmael/Icons/Middle/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Violet~ Breaking Slam", icon: "IDs/Ishmael/Icons/Middle/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "I'll Get You Back for This, Man", icon: "IDs/Ishmael/Icons/Middle/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     JeongIshmael: [
         { skillName: "", icon: "", skillNum: "" },
