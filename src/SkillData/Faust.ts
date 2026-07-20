a3a3a733a66d3e400230222b651b7ad93c66abab
Jokeren1 <inhakim999@gmail.com>
2026-07-20 15:04:45 -0500

    Update Heathcliff voiceline and add Faust Dawn ID

--- src/SkillData/Faust.ts
+++ src/SkillData/Faust.ts
@@ -37,6 +37,15 @@ export const faustskillData = {
         { skillName: "Red Plum Blossoms Scatter", icon: "IDs/Faust/Icons/Blade/Skill3.webp", skillNum: "Skill 3" },
         { skillName: "Redirect", icon: "IDs/Faust/Icons/Blade/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
+    DawnFaust: [
+        { skillName: "Flash of Sunup", icon: "IDs/Faust/Icons/Dawn/WIP.png", skillNum: "Skill 1" },
+        { skillName: "Seconded Offensive", icon: "IDs/Faust/Icons/Dawn/WIP.png", skillNum: "Skill 1-2" },
+        { skillName: "Butterfly Slash", icon: "IDs/Faust/Icons/Dawn/WIP.png", skillNum: "Skill 2" },
+        { skillName: "Eject", icon: "IDs/Faust/Icons/Dawn/WIP.png", skillNum: "Skill 3" },
+        { skillName: "Eject - High Noon", icon: "IDs/Faust/Icons/Dawn/WIP.png", skillNum: "Skill 3-2" },
+        { skillName: "Stigmatize", icon: "IDs/Faust/Icons/Dawn/WIP.png", skillNum: "Defense" },
+        { skillName: "Dismemberment at High Noon", icon: "IDs/Faust/Icons/Dawn/WIP.png", skillNum: "Defense 2" }
+    ] satisfies Skill[],
     HeishouFaust: [
         { skillName: "Blinkstep", icon: "IDs/Faust/Icons/Heishou/Skill1.webp", skillNum: "Skill 1" },
         { skillName: "Clearing the Path, My Lord.", icon: "IDs/Faust/Icons/Heishou/Skill2.webp", skillNum: "Skill 2" },
