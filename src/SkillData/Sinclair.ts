886f7299646d9e7ac69052c1cf1d88fad4c8ec96
Jokeren1 <inhakim999@gmail.com>
2026-07-21 11:29:08 -0500

    Implemented Sinclair's skills and added voicelines

--- src/SkillData/Sinclair.ts
+++ src/SkillData/Sinclair.ts
@@ -2,93 +2,99 @@ import type { Skill } from "../type.js";
 
 export const sinclairskillData = {
     LCBSinclair: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Downward Swing", icon: "IDs/Sinclair/Skills/LCB/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Halberd combo", icon: "IDs/Sinclair/Skills/LCB/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Ravaging Cut", icon: "IDs/Sinclair/Skills/LCB/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Counter", icon: "IDs/Sinclair/Skills/LCB/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     RedSinclair: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Magnify Wound", icon: "IDs/Sinclair/Skills/Red/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Poliferating Talismans", icon: "IDs/Sinclair/Skills/Red/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Rupturing Talisman", icon: "IDs/Sinclair/Skills/Red/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Guard", icon: "IDs/Sinclair/Skills/Red/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     JefeSinclair: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Baile y Rola", icon: "IDs/Sinclair/Skills/Jefe/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Danza de Pasión", icon: "IDs/Sinclair/Skills/Jefe/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Pañata Party", icon: "IDs/Sinclair/Skills/Jefe/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Evade", icon: "IDs/Sinclair/Skills/Jefe/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     MolarSinclair: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Fierce Assault", icon: "IDs/Sinclair/Skills/Molar/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Steady...", icon: "IDs/Sinclair/Skills/Molar/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Gamble", icon: "IDs/Sinclair/Skills/Molar/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Crab Bastards...!", icon: "IDs/Sinclair/Skills/Molar/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     ZweiSSinclair: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Baton", icon: "IDs/Sinclair/Skills/ZweiS/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Suppress", icon: "IDs/Sinclair/Skills/ZweiS/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Strong Strike", icon: "IDs/Sinclair/Skills/ZweiS/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Guard", icon: "IDs/Sinclair/Skills/ZweiS/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     ZweiWSinclair: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Suppressing.", icon: "IDs/Sinclair/Skills/ZweiW/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Combat Preparation", icon: "IDs/Sinclair/Skills/ZweiW/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Fence", icon: "IDs/Sinclair/Skills/ZweiW/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Zwei Knight's Greatsword Form - Guard", icon: "IDs/Sinclair/Skills/ZweiW/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     BladeSinclair: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Slice then Stab", icon: "IDs/Sinclair/Skills/Blade/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Slash Series", icon: "IDs/Sinclair/Skills/Blade/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "To Claim Their Bones", icon: "IDs/Sinclair/Skills/Blade/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Newsprung Counter", icon: "IDs/Sinclair/Skills/Blade/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     CinqSinclair: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Remise", icon: "IDs/Sinclair/Skills/Cinq/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Engagement", icon: "IDs/Sinclair/Skills/Cinq/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Contre Attaque", icon: "IDs/Sinclair/Skills/Cinq/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Défensive", icon: "IDs/Sinclair/Skills/Cinq/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     DawnSinclair: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Fierce Charge", icon: "IDs/Sinclair/Skills/Dawn/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Fierce Charge", icon: "IDs/Sinclair/Skills/Dawn/Skill1_2.webp", skillNum: "Skill 1-EGO" },
+        { skillName: "Sunset Blade", icon: "IDs/Sinclair/Skills/Dawn/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Sunset Blade", icon: "IDs/Sinclair/Skills/Dawn/Skill2_2.webp", skillNum: "Skill 2-EGO" },
+        { skillName: "Stigmatize", icon: "IDs/Sinclair/Skills/Dawn/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Blazing Strike", icon: "IDs/Sinclair/Skills/Dawn/Skill3_2.webp", skillNum: "Skill 3-2" },
+        { skillName: "Rue", icon: "IDs/Sinclair/Skills/Dawn/Defense.webp", skillNum: "Defense" },
+        { skillName: "Rue", icon: "IDs/Sinclair/Skills/Dawn/Defense2.webp", skillNum: "Defense-EGO" }
     ] satisfies Skill[],
     DevyatSinclair: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Courier Trunk - Decay Hammer", icon: "IDs/Sinclair/Skills/Devyat/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Courier Trunk - Demolition Gadget", icon: "IDs/Sinclair/Skills/Devyat/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Poludnitsa… I trust you!", icon: "IDs/Sinclair/Skills/Devyat/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Courier Assistance & Crisis Control System", icon: "IDs/Sinclair/Skills/Devyat/Defense.webp", skillNum: "Defense" },
+        { skillName: "W-wait, I need to do some maintenance…!", icon: "IDs/Sinclair/Skills/Devyat/Defense2.webp", skillNum: "Defense 2" }
     ] satisfies Skill[],
     HeishouSinclair: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Oh, Let the Smell of Burning Blood…", icon: "IDs/Sinclair/Skills/Heishou/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "… Fuel My Rampage.", icon: "IDs/Sinclair/Skills/Heishou/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Bloodflame Massacre [血炎亂舞]", icon: "IDs/Sinclair/Skills/Heishou/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Rooster's Deathstrike [鷄窮則殺]", icon: "IDs/Sinclair/Skills/Heishou/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     PinkySinclair: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Chainstrike [連擊]", icon: "IDs/Sinclair/Skills/Pinky/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Nameless Slash [無名斬]", icon: "IDs/Sinclair/Skills/Pinky/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Fallstar Slayer [落星一殺]", icon: "IDs/Sinclair/Skills/Pinky/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Defend [守]", icon: "IDs/Sinclair/Skills/Pinky/Defense.webp", skillNum: "Defense" },
+        { skillName: "Swerve [回]", icon: "IDs/Sinclair/Skills/Pinky/Defense2.webp", skillNum: "Defense 2" }
     ] satisfies Skill[],
     MiddleSinclair: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Is it You?!", icon: "IDs/Sinclair/Skills/Middle/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Payback with Interest", icon: "IDs/Sinclair/Skills/Middle/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Write 'em all down", icon: "IDs/Sinclair/Skills/Middle/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Warmup in the East", icon: "IDs/Sinclair/Skills/Middle/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     NCorpSinclair: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Coerced Judgement", icon: "IDs/Sinclair/Skills/NCorp/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Amoral Enactment", icon: "IDs/Sinclair/Skills/NCorp/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Self-destructive Purge", icon: "IDs/Sinclair/Skills/NCorp/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Enough...", icon: "IDs/Sinclair/Skills/NCorp/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[],
     ThumbSinclair: [
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
-        { skillName: "", icon: "", skillNum: "" },
+        { skillName: "Adherence to Propriety.", icon: "IDs/Sinclair/Skills/Thumb/Skill1.webp", skillNum: "Skill 1" },
+        { skillName: "Respect the Thumb.", icon: "IDs/Sinclair/Skills/Thumb/Skill2.webp", skillNum: "Skill 2" },
+        { skillName: "Thruster Compression", icon: "IDs/Sinclair/Skills/Thumb/Skill3.webp", skillNum: "Skill 3" },
+        { skillName: "Ferrous Guard", icon: "IDs/Sinclair/Skills/Thumb/Defense.webp", skillNum: "Defense" }
     ] satisfies Skill[]
 };
